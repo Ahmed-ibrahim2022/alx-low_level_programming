9c7824299221db4030cd2c805a954778ec627831
Ahmed Ibrahim <100896906+Ahmed-ibrahim2022@users.noreply.github.com>
2022-09-12 23:59:47 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ * main - Entry point
+ *
+ * Description: 'print using puts'
+ *
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
