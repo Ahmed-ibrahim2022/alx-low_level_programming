2d3545942567f9062ff0a99df10c253cf327efef
Ahmed Ibrahim <100896906+Ahmed-ibrahim2022@users.noreply.github.com>
2022-09-13 21:59:48 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,18 +7,14 @@
 */
 void print_alphabet_x10(void)
 {
-int alphabet;
-int count;
+char i;
+int count = 0;
 
-
-count = 0;
 while (count < 10)
 {
-for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
-}
-
-count++;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
 _putchar('\n');
+count++;
+}
 }
