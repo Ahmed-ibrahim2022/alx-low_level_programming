684037bb869c98d1e11996ab9ecab8d4a358a7f0
Ahmed Ibrahim <100896906+Ahmed-ibrahim2022@users.noreply.github.com>
2022-09-13 22:07:48 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- *  * _islower - Short description, single line
- *   * @c: contains value to be compared
- *    * Return: Always 0.
- *     */
+ * _islower - Short description, single line
+ * 
+ * Return: Always 0.
+ */
 int _islower(int c)
 
 {
 
-	if (c > 'a' && c < 'z')
-	{
-		return (1);
-	}
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
 
-	else
-	{
-		return (0);
-	}
+else
+{
+return (0);
+}
 
 }
