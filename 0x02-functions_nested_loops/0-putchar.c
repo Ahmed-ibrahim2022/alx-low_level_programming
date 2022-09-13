fe6fc74f47acc50e1d7e5d190f74840b5716a2d1
Ahmed Ibrahim <100896906+Ahmed-ibrahim2022@users.noreply.github.com>
2022-09-13 17:16:59 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,22 +2,22 @@
 
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: nothing, return void
- *     */
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
