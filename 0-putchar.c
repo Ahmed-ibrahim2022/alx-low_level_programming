3dc2846c9f1fd1a898e76d11b50c8e23547d3d15
Ahmed Ibrahim <100896906+Ahmed-ibrahim2022@users.noreply.github.com>
2022-09-13 17:12:32 +0200

    Update 0-putchar.c

--- 0-putchar.c
+++ 0-putchar.c
@@ -1,23 +1,19 @@
-#include "main.h"
-
+#include "holberton.h"
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: nothing, return void
- *     */
+ * main - Entry point
+ * Return:0
+ */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+char c[10] = "Holberton";
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
