052c919a0b1d8e56e3453691bed060ed07959799
Ahmed Ibrahim <100896906+Ahmed-ibrahim2022@users.noreply.github.com>
2022-09-13 04:54:12 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * * main - Entry point
- * *
- * * description: 'print every number comination'
- * *
- * * Return: Always 0 (Success)
- * */
+* main - Entry point
+*
+* description: 'print every number comination'
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-		int i, j;
+	int i, j;
 
-			for (i = 48; i < 58; i++)
-					{
-								for (j = i + 1; j < 58; j++)
-											{
-															putchar(i);
-																		putchar(j);
-																					if (i == 56 && j == 57)
-																									{
-																														break;
-																																	}
-																									putchar(',');
-																													putchar(' ');
-																															}
-									}
-				putchar('\n');
-					return (0);
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == 56 && j == 57)
+			{
+				break;
+			}
+				putchar(',');
+				putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
