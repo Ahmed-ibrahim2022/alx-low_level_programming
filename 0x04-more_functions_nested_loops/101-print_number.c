973b1512e2acb887bf4280fa4694f1f43074ae6e
Ahmed Ibrahim <100896906+Ahmed-ibrahim2022@users.noreply.github.com>
2022-09-16 03:07:01 +0200

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * * print_number - that prints an integer
- * *@n: print int
- * * Return: Always 0.
- * */
+* print_number - that prints an integer
+*@n: print int
+* Return: Always 0.
+*/
 void print_number(int n)
 {
