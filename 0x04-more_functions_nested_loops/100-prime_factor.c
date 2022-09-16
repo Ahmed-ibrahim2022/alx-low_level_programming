167bee69b2f73e8ac7655d2b1dbd358596e95f51
Ahmed Ibrahim <100896906+Ahmed-ibrahim2022@users.noreply.github.com>
2022-09-16 03:02:41 +0200

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 
 {
-	long n, i;
+long n, i;
 
-	n = 612852475143;
-	for (i = 2; i < n; i++)
-	{
-		while (n % i == 0)
-			n = n / i;
-	}
+n = 612852475143;
+for (i = 2; i < n; i++)
+{
+while (n % i == 0)
+n = n / i;
+}
 
-	printf("%lu\n", n);
+printf("%lu\n", n);
 
-	return (0);
+return (0);
 }
