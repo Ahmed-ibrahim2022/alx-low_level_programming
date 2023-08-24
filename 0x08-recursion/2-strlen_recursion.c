4a5dac1d21675008d2d188fb4f38a8f4a9a71efe
Ahmed Ibrahim <y7ya5528@gmail.com>
2023-08-24 08:37:07 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
+int main(void)
 {
-if (*s == '\0') {
-return 0;
-} else {
-return 1 + _strlen_recursion(s + 1);
-}
+int n;
+n = _strlen_recursion("You mustn't be afraid to dream a little bigger, darling.");
+printf("%d\n", n);
+return (0);
 }
