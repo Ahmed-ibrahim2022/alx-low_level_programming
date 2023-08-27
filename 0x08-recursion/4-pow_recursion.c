b08246896c05b847c61d6201a7cebf3e6f4b2ab1
Ahmed Ibrahim <y7ya5528@gmail.com>
2023-08-27 14:17:42 +0300

    Update

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* _pow_recursion_recursive-helper function > calculating power using recursion.
+*
+* @x: base.
+* @y: exponent.
+* Return: result of x raised to the power of y.
+*/
+int _pow_recursion_recursive(int x, int y)
+{
+if (y < 0)
+return (-1);
+else if (y != 0)
+return (x * _pow_recursion_recursive(x, y - 1));
+else
+return (1);
+}
+
+/**
+* _pow_recursion - returns the value of x raised to the power of y.
+*
+* @x: integer.
+* @y: power.
+* Return: result of x raised to the power of y.
+*/
+int _pow_recursion(int x, int y)
+{
+return _pow_recursion_recursive(x, y);
+}
