478de59a217728f313989631c3b173a82d3b4c55
Ahmed Ibrahim <y7ya5528@gmail.com>
2023-08-27 19:20:13 +0300

    Refactor palindrome check with recursive approach

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+* _strlen_recursive - calculate the length of a string recursively.
+* @s: pointer to string parameter.
+* Return: length of the string.
+*/
+int _strlen_recursive(char *s)
+{
+if (!*s)
+{
+return (0);
+}
+return (1 + _strlen_recursive(++s));
+}
+
+/**
+* is_palindrome_recursive-check if string is palindrome recursively.
+* @s: pointer to string parameter.
+* @l: length of the string.
+* Return: 1 if palindrome, 0 otherwise.
+*/
+int is_palindrome_recursive(char *s, int l)
+{
+if (l < 1)
+{
+return (1);
+}
+    
+if (*s == *(s + l))
+{
+ return (is_palindrome_recursive(s + 1, l - 2));
+}
+return ((0));
+}
+
+/**
+* is_palindrome-check if a string is palindrome using recursion.
+* @s: pointer to string parameter.
+* Return: 1 if palindrome, 0 otherwise.
+*/
+int is_palindrome(char *s)
+{
+int len = _strlen_recursive(s);
+return (is_palindrome_recursive(s, len - 1));
+}
