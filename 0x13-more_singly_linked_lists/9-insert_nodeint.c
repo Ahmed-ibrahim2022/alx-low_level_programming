abb1838a51be7cad93a4d25f1741034bf3b30c3b
Ahmed Ibrahim <y7ya5528@gmail.com>
2023-08-28 23:46:23 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,11 +3,10 @@
 
 /**
 * insert_nodeint_at_index - Insert a new node at a given position in the list.
-* @head: Pointer to the listint_t head* @idx: Index at which to insert the new node
+* @head: Pointer to the listint_t head* @idx: Index at which >insert new node
 * @n: Value for the new node
 * Return: Address of the new node, or NULL if failed
 */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *current = *head;
