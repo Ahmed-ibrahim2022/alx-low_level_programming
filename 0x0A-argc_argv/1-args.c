eb2b1e65b758e81b60c9469110d8cef7fd6de84c
Ahmed Ibrahim <y7ya5528@gmail.com>
2023-08-29 12:48:47 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,22 +6,12 @@
 *
 * @arg_count: Number of command-line arguments
 */
-void print_argument_count(int arg_count)
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-printf("%i\n", arg_count);
-}
-
-/**
-* main - Entry point
-*
-* @argc: Counts the number of parameters that go into main
-* @argv: Pointer>array>pointers containing strings entering main
-* Return: Always 0 (Success)
-*/
-int main(int argc, char **argv)
+printf("%d\n", argc - 1);
+return (0);
+}int main(int argc, char *argv[] __attribute__((unused)))
 {
-(void) argv; // Unused parameter, suppresses compiler warning
-int argument_count = argc - 1; // Calculate the number>arguments
-print_argument_count(argument_count);
-return 0;
+printf("%d\n", argc - 1);
+return (0);
 }
